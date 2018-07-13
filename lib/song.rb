2a819170253c9944c3011d17ba56e12f5ad511f1
shannonbee <shannonbenyamin@gmail.com>
2018-07-13 20:28:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
-
+class Song
+attr_accessor :name, :artist, :genres
+@@count = 0 
+@@artist = [ ]
+@@genres = [ ]
+def initialization (name, artist, genre)
+  @name = name
+  @artist = artist 
+  @@genre = genre
+  @@count +=1
+  @@genres << genre
+  @@artists << artist
+  @@Song.count
+Song.count
+Song.artists
+Song.genres
+Song.genre_count
+{"rap"=>}
